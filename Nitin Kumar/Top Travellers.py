2fe69daebc18b89d898bb4f80194d1f848390cd7
Sahnawaz Shaban <shaanshaban400@gmail.com>
2024-12-08 15:16:50 +0530

    Add new Query using CASE statement

--- Nitin Kumar/Top Travellers.py
+++ Nitin Kumar/Top Travellers.py
@@ -100,4 +100,25 @@ From Users u  LEFT JOIN RIDES r
 ON u.id = r.user_id
 GROUP BY u.id
 ORDER BY travelled_distance DESC, u.name;
+
+-- SOLUTION - 3
+SELECT 
+    u.name, 
+    SUM(
+        CASE
+            When distance is null then 0
+            else distance
+        END
+    ) AS travelled_distance 
+FROM 
+    Users u
+LEFT JOIN 
+    Rides r
+ON 
+    u.id = r.user_id
+GROUP BY 
+    u.id, u.name
+ORDER BY 
+    travelled_distance DESC, name;
+
 '''
